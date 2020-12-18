3651c9cf6ebf641af943c33f23923d7b6af4862a
13233 <1323334891@qq.com>
2020-12-18 19:12:57 +0800

    6

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,9 +27,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        donghua();
-        getTime();
-        jianting();
 
 
     }
